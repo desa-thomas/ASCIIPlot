1fa6f95fc90c82dc84cd6bf54a3b216c8b37003d
tom <desa2thomas@gmail.com>
2025-07-22 23:35:09 -0400

    initialized ncurses

--- src/main.c
+++ src/main.c
@@ -3,9 +3,40 @@ Desmos in your terminal, you absolutely don't need it
 */
 
 #include <ncurses.h>
+#include <stdbool.h>
+#include "cartesian-plane.h"
 
 int main() {
 
-    printf("Hello world"); 
+    /* Initialize ncurses */
+
+    initscr();              //init screen
+    raw();                  //enable raw mode (how to handle ctrl c)
+    noecho();               //disable echoing
+    keypad(stdscr, true);   //enable keypad
+    curs_set(0);            //make cursor invisible
+    //nodelay(stdscr, true); //make getch non-blocking
+
+    /* Initalize cartesian plane */
+    Plane *p = init_plane(100, 30, 5.0, 75, 20); 
+
+    int ch;
+    while (true)
+    {
+        draw_plane(p);
+
+        ch = getch(); 
+        if (ch == 3)
+        {
+            break;
+        }
+        else 
+        {
+            //pass
+        }
+        
+    }
+
+    endwin();
     return 0;
 }
